cc2693213dc5e99f2ab21886b9073419ded9195b
almerlucke <almer.lucke@gmail.com>
2022-11-04 16:15:54 +0100

    added resources and markov rhythm example

--- examples/beats/main.go
+++ examples/beats/main.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/almerlucke/muse/modules/player"
 	"github.com/almerlucke/muse/value"
+	"github.com/almerlucke/muse/value/markov"
 	"github.com/almerlucke/muse/value/template"
 )
 
@@ -35,6 +36,112 @@ func addDrumTrack(env *muse.Environment, moduleName string, soundBuffer *io.Soun
 	return env.AddModule(player.NewPlayer(soundBuffer, 1.0, amp, true, env.Config, moduleName))
 }
 
+func kickRhythm() value.Valuer[*swing.Step] {
+	rhythm1 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm2 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm3 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true},
+	}, nil)
+
+	rhythm4 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true},
+		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true},
+	}, nil)
+
+	rhythm5 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {}, {Skip: true},
+		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true},
+	}, nil)
+
+	rhythm1.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm1, 3.0, rhythm2, 2.0, rhythm3, 1.0)
+	rhythm2.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm2, 3.0, rhythm3, 2.0, rhythm4, 1.0)
+	rhythm3.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm3, 3.0, rhythm4, 2.0, rhythm5, 1.0)
+	rhythm4.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm4, 4.0, rhythm5, 2.0, rhythm1, 1.0)
+	rhythm5.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm5, 4.0, rhythm1, 2.0, rhythm2, 1.0)
+
+	m := markov.NewMarkov[[]*swing.Step](markov.NewStateStarter(rhythm1), 1)
+
+	return value.NewFlatten[*swing.Step](m)
+}
+
+func snareRhythm() value.Valuer[*swing.Step] {
+	rhythm1 := markov.NewState([]*swing.Step{
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm2 := markov.NewState([]*swing.Step{
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm3 := markov.NewState([]*swing.Step{
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true},
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {BurstChance: 0.5, NumBurst: 3}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm4 := markov.NewState([]*swing.Step{
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {},
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {BurstChance: 0.5, NumBurst: 3}, {Skip: true}, {Skip: true}, {Skip: true},
+	}, nil)
+
+	rhythm5 := markov.NewState([]*swing.Step{
+		{Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {BurstChance: 0.5, NumBurst: 3},
+		{Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {Skip: true}, {},
+	}, nil)
+
+	rhythm1.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm1, 3.0, rhythm2, 2.0, rhythm3, 1.0)
+	rhythm2.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm2, 3.0, rhythm3, 2.0, rhythm4, 1.0)
+	rhythm3.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm3, 3.0, rhythm4, 2.0, rhythm5, 1.0)
+	rhythm4.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm4, 4.0, rhythm5, 2.0, rhythm1, 1.0)
+	rhythm5.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm5, 4.0, rhythm1, 2.0, rhythm2, 1.0)
+
+	m := markov.NewMarkov[[]*swing.Step](markov.NewStateStarter(rhythm1), 1)
+
+	return value.NewFlatten[*swing.Step](m)
+}
+
+func hihatRhythm() value.Valuer[*swing.Step] {
+	rhythm1 := markov.NewState([]*swing.Step{
+		{}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true},
+	}, nil)
+
+	rhythm2 := markov.NewState([]*swing.Step{
+		{BurstChance: 0.5, NumBurst: 3}, {Shuffle: 0.1}, {}, {Skip: true}, {}, {Shuffle: 0.1}, {}, {Skip: true},
+	}, nil)
+
+	rhythm3 := markov.NewState([]*swing.Step{
+		{BurstChance: 0.5, NumBurst: 3}, {Shuffle: 0.1}, {Skip: true}, {Shuffle: 0.1}, {}, {Shuffle: 0.1}, {Skip: true}, {Shuffle: 0.1},
+	}, nil)
+
+	rhythm4 := markov.NewState([]*swing.Step{
+		{BurstChance: 0.5, NumBurst: 3}, {Skip: true}, {}, {Shuffle: 0.1}, {}, {Skip: true}, {}, {Shuffle: 0.1},
+	}, nil)
+
+	rhythm5 := markov.NewState([]*swing.Step{
+		{}, {Shuffle: 0.1}, {}, {Shuffle: 0.1}, {}, {Shuffle: 0.1}, {BurstChance: 0.5, NumBurst: 3}, {Shuffle: 0.1},
+	}, nil)
+
+	rhythm1.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm1, 2.0, rhythm2, 1.0, rhythm3, 1.0)
+	rhythm2.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm2, 2.0, rhythm3, 1.0, rhythm4, 1.0)
+	rhythm3.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm3, 2.0, rhythm4, 1.0, rhythm5, 1.0)
+	rhythm4.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm4, 3.0, rhythm5, 1.0, rhythm1, 1.0)
+	rhythm5.Transitioner = markov.NewProbabilityTransitionerVariadic[[]*swing.Step](rhythm5, 3.0, rhythm1, 1.0, rhythm2, 1.0)
+
+	m := markov.NewMarkov[[]*swing.Step](markov.NewStateStarter(rhythm1), 1)
+
+	return value.NewFlatten[*swing.Step](m)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	env := muse.NewEnvironment(1, 44100.0, 512)
@@ -42,30 +149,26 @@ func main() {
 	bpm := 80
 
 	hihatSound, _ := io.NewSoundFileBuffer("resources/drums/hihat/Cymatics - Humble Closed Hihat 1.wav")
-	kickSound, _ := io.NewSoundFileBuffer("resources/drums/kick/Cymatics - Humble Sit Down Kick - D.wav")
-	snareSound, _ := io.NewSoundFileBuffer("resources/drums/snare/Cymatics - Humble Theory Snare - D#.wav")
+	kickSound, _ := io.NewSoundFileBuffer("resources/drums/kick/Cymatics - Humble Triple Kick - E.wav")
+	snareSound, _ := io.NewSoundFileBuffer("resources/drums/shots/Cymatics - Orchid Snap - Single.wav")
+	shakerSound, _ := io.NewSoundFileBuffer("resources/drums/shots/Cymatics - Orchid Snap - Cream.wav")
+	fx1Sound, _ := io.NewSoundFileBuffer("resources/drums/shots/Cymatics - Orchid Shaker - Drew.wav")
 
-	hihatPlayer := addDrumTrack(env, "hihat", hihatSound, bpm, 4, 0.875, 1.125, 1.0, value.NewSequence([]*swing.Step{
-		{Skip: true}, {}, {Skip: true}, {Shuffle: 0.05, ShuffleRand: 0.05}, {Skip: true}, {BurstChance: 0.6, NumBurst: 3}, {Skip: true},
-		{}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true},
-		{}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true},
-		{}, {Skip: true}, {}, {Skip: true}, {Multiply: 0.25}, {Multiply: 0.25}, {Multiply: 0.25}, {Multiply: 0.25},
-		{}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true},
-		{}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {}, {Skip: true}, {},
-	}))
+	hihatPlayer := addDrumTrack(env, "hihat", hihatSound, bpm, 4, 0.875, 1.125, 0.6, hihatRhythm())
 
-	kickPlayer := addDrumTrack(env, "kick", kickSound, bpm, 4, 0.875, 1.125, 1.0, value.NewSequence([]*swing.Step{
-		{}, {Skip: true},
-	}))
+	kickPlayer := addDrumTrack(env, "kick", kickSound, bpm, 8, 0.875, 1.125, 1.0, kickRhythm())
 
-	snarePlayer := addDrumTrack(env, "snare", snareSound, bpm, 4, 0.875, 1.125, 1.0, value.NewSequence([]*swing.Step{
-		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true},
-		{Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {}, {Skip: true}, {Skip: true}, {Skip: true}, {BurstChance: 0.6, NumBurst: 3}, {Skip: true},
-	}))
+	snarePlayer := addDrumTrack(env, "snare", snareSound, bpm, 8, 1.0, 1.0, 1.0, snareRhythm())
+
+	shakerPlayer := addDrumTrack(env, "shaker", shakerSound, bpm, 2, 1.0, 1.0, 1.0, snareRhythm())
+
+	fx1Player := addDrumTrack(env, "fx1", fx1Sound, bpm, 2, 1.0, 1.0, 1.0, kickRhythm())
 
 	kickPlayer.Connect(0, env, 0)
 	hihatPlayer.Connect(0, env, 0)
 	snarePlayer.Connect(0, env, 0)
+	shakerPlayer.Connect(0, env, 0)
+	fx1Player.Connect(0, env, 0)
 
 	env.QuickPlayAudio()
 }
